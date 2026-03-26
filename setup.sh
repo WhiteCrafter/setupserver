e8810a6769d88583e26d1319a3ba741c0df8f573
WhiteCrafter <65396435+WhiteCrafter@users.noreply.github.com>
2026-03-26 13:12:39 +0400

    Add cloudflared installation steps to setup.sh
    Added installation steps for cloudflared including GPG key and repository.

--- setup.sh
+++ setup.sh
@@ -60,3 +60,15 @@ sudo systemctl start cloudflared
 sudo cloudflared service install eyJhIjoiOGJlMjA4YWY2MGM3MTA3YTkwYTBmZDNhNGJmNTljMDIiLCJ0IjoiOTEwZjI1YzMtZjg1NS00Y2E3LTlmYmEtODg2NDdjNTUyZWZhIiwicyI6IlpEWXhNRFF5WTJFdE1UTXhPQzAwWW1ZeUxUZ3hNMk10WkRFME16UmpZelE0TVRJNSJ9
 
 
+# Add cloudflare gpg key
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-public-v2.gpg | sudo tee /usr/share/keyrings/cloudflare-public-v2.gpg >/dev/null
+
+# Add this repo to your apt repositories
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-public-v2.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+# install cloudflared
+sudo apt-get update && sudo apt-get install cloudflared
+
+
+
